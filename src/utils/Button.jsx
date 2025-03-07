483c75ff2cc9db6f08bfef24b46dc9bc3b41a9bd
ravi <software.tarinee1@brihaspathi.com>
2025-03-07 17:21:24 +0530

    card added

--- src/utils/Button.jsx
+++ src/utils/Button.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BeatLoader } from "react-spinners";
 
-const Button = ({ type, text, isLoading }) => {
+const Button = ({ type, text, isLoading, width = "250px", onClick }) => {
   return (
     <button
+      style={{ width }}
       type={type}
+      onClick={onClick}
       className="w-[250px] md:w-[360px] min-h-[45px] bg-[#EA4c89] text-white rounded-md"
     >
       {isLoading ? <BeatLoader color="#fff" size={10} /> : text}
